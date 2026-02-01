fce997097c682542bbf73283913f4eff6661cf2f
vladkoniakhin <vlad.koniakhin@gmail.com>
2026-02-01 23:04:13 +0100

    Updated.

--- src/System.cpp
+++ src/System.cpp
@@ -90,37 +90,110 @@ void SystemController::runWorkerLoop() {
 }
 
 void SystemController::processCommand(CommandMessage cmd) {
+    // 1. Обработка выбора цели (это не атака, можно делать всегда)
     if (cmd.cmd == SystemCommand::CMD_SELECT_TARGET) {
         auto results = _wifiEngine.getScanResults();
         if (cmd.param1 >= 0 && cmd.param1 < (int)results.size()) _selectedTarget = results[cmd.param1];
         return;
     }
 
-    stopCurrentTask();
+    // 2. Команда остановки всегда имеет приоритет
+    if (cmd.cmd == SystemCommand::CMD_STOP_ATTACK) {
+        stopCurrentTask();
+        return;
+    }
 
+    // 3. АДМИН-ПАНЕЛЬ (Web)
+    // Разрешаем вход в админку, только если ничего не запущено
+    if (cmd.cmd == SystemCommand::CMD_START_ADMIN_MODE) {
+        if (_activeEngine != nullptr) {
+            // ОШИБКА: Нельзя войти в админку во время атаки
+            return; 
+        }
+        _currentState = SystemState::ADMIN_MODE;
+        WebPortalManager::getInstance().start("nRF_Admin");
+        return;
+    }
+
+    // 4. ЗАЩИТА ОТ BROWNOUT (Самое важное!)
+    // Если уже работает какой-то Engine (WiFi, NRF, SubGhz), 
+    // мы ЗАПРЕЩАЕМ запуск нового. Пользователь обязан нажать STOP.
+    if (_activeEngine != nullptr) {
+        // Тут можно добавить отправку сообщения на экран "BUSY! PRESS STOP"
+        // Но пока просто игнорируем команду, чтобы не спалить питание
+        Serial.println("[Error] System Busy. Stop current attack first!");
+        return; 
+    }
+
+    // 5. Запуск новых задач (только если _activeEngine == nullptr)
     switch (cmd.cmd) {
-        case SystemCommand::CMD_STOP_ATTACK: _currentState = SystemState::IDLE; break;
+        case SystemCommand::CMD_START_SCAN_WIFI: 
+            _activeEngine = &_wifiEngine; 
+            _wifiEngine.startScan(); 
+            break;
+
+        case SystemCommand::CMD_START_DEAUTH: 
+            if (_selectedTarget.bssid[0] != 0) { 
+                _activeEngine = &_wifiEngine; 
+                _wifiEngine.startDeauth(_selectedTarget); 
+            } 
+            break;
+
+        case SystemCommand::CMD_START_EVIL_TWIN: 
+            if (_selectedTarget.bssid[0] != 0) { 
+                _activeEngine = &_wifiEngine; 
+                _wifiEngine.startEvilTwin(_selectedTarget); 
+            } 
+            break;
+
+        case SystemCommand::CMD_START_BEACON_SPAM: 
+            _activeEngine = &_wifiEngine; 
+            _wifiEngine.startBeaconSpam(); 
+            break;
             
-        case SystemCommand::CMD_START_SCAN_WIFI: _activeEngine = &_wifiEngine; _wifiEngine.startScan(); break;
-        case SystemCommand::CMD_START_DEAUTH: if (_selectedTarget.bssid[0] != 0) { _activeEngine = &_wifiEngine; _wifiEngine.startDeauth(_selectedTarget); } break;
-        case SystemCommand::CMD_START_EVIL_TWIN: if (_selectedTarget.bssid[0] != 0) { _activeEngine = &_wifiEngine; _wifiEngine.startEvilTwin(_selectedTarget); } break;
-        case SystemCommand::CMD_START_BEACON_SPAM: _activeEngine = &_wifiEngine; _wifiEngine.startBeaconSpam(); break;
+        case SystemCommand::CMD_START_BLE_SPOOF: 
+            _activeEngine = &BleManager::getInstance(); 
+            BleManager::getInstance().startSpoof((BleSpoofType)cmd.param1); 
+            break;
             
-        case SystemCommand::CMD_START_BLE_SPOOF: _activeEngine = &BleManager::getInstance(); BleManager::getInstance().startSpoof((BleSpoofType)cmd.param1); break;
+        case SystemCommand::CMD_START_NRF_JAM: 
+            _activeEngine = &NrfManager::getInstance(); 
+            NrfManager::getInstance().startJamming((uint8_t)cmd.param1); 
+            break;
+
+        case SystemCommand::CMD_START_NRF_ANALYZER: 
+            _activeEngine = &NrfManager::getInstance(); 
+            NrfManager::getInstance().startAnalyzer(); 
+            break;
+
+        case SystemCommand::CMD_START_MOUSEJACK: 
+            _activeEngine = &NrfManager::getInstance(); 
+            NrfManager::getInstance().startMouseJack(0); 
+            break;
+
+        case SystemCommand::CMD_START_NRF_SNIFF: 
+            _activeEngine = &NrfManager::getInstance(); 
+            NrfManager::getInstance().startSniffing(); 
+            break;
             
-        case SystemCommand::CMD_START_NRF_JAM: _activeEngine = &NrfManager::getInstance(); NrfManager::getInstance().startJamming((uint8_t)cmd.param1); break;
-        case SystemCommand::CMD_START_NRF_ANALYZER: _activeEngine = &NrfManager::getInstance(); NrfManager::getInstance().startAnalyzer(); break;
-        case SystemCommand::CMD_START_MOUSEJACK: _activeEngine = &NrfManager::getInstance(); NrfManager::getInstance().startMouseJack(0); break;
-        case SystemCommand::CMD_START_NRF_SNIFF: _activeEngine = &NrfManager::getInstance(); NrfManager::getInstance().startSniffing(); break;
-            
-        case SystemCommand::CMD_START_SUBGHZ_SCAN: _activeEngine = &SubGhzManager::getInstance(); SubGhzManager::getInstance().startAnalyzer(); break;
-        case SystemCommand::CMD_START_SUBGHZ_JAM: _activeEngine = &SubGhzManager::getInstance(); SubGhzManager::getInstance().startJammer(); break;
-        case SystemCommand::CMD_START_SUBGHZ_RX: _activeEngine = &SubGhzManager::getInstance(); SubGhzManager::getInstance().startCapture(); break;
-        case SystemCommand::CMD_START_SUBGHZ_TX: _activeEngine = &SubGhzManager::getInstance(); SubGhzManager::getInstance().startReplay(); break;
-            
-        case SystemCommand::CMD_START_ADMIN_MODE: 
-            _currentState = SystemState::ADMIN_MODE; 
-            WebPortalManager::getInstance().start("nRF_Admin"); 
+        case SystemCommand::CMD_START_SUBGHZ_SCAN: 
+            _activeEngine = &SubGhzManager::getInstance(); 
+            SubGhzManager::getInstance().startAnalyzer(); 
+            break;
+
+        case SystemCommand::CMD_START_SUBGHZ_JAM: 
+            _activeEngine = &SubGhzManager::getInstance(); 
+            SubGhzManager::getInstance().startJammer(); 
+            break;
+
+        case SystemCommand::CMD_START_SUBGHZ_RX: 
+            _activeEngine = &SubGhzManager::getInstance(); 
+            SubGhzManager::getInstance().startCapture(); 
+            break;
+
+        case SystemCommand::CMD_START_SUBGHZ_TX: 
+            _activeEngine = &SubGhzManager::getInstance(); 
+            SubGhzManager::getInstance().startReplay(); 
             break;
             
         default: break;
